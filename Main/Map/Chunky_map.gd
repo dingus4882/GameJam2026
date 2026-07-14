cbfaf71f272b338392b7e6a30725d9a588508b07
FlameLane <flamelane67@gmail.com>
2026-07-14 12:39:53 +0700

    scale of everything changed

--- Main/Map/Chunky_map.gd
+++ Main/Map/Chunky_map.gd
@@ -10,35 +10,47 @@ extends TileMapLayer
 @export var tile_ratio : Vector2i = Vector2i (32,32)
 @export var chunk_size: Vector2i = Vector2i (16,16)
 @export var render_distance: Vector2i = Vector2i (3,3)
-@export var off_set: Vector2i = Vector2i (-1,-1)
+@export var chunk_off_set: Vector2i = Vector2i (-1,-1)
 
 var layers_data: Array[TileMapLayer]
 
 
 var scene_path = scene_file_path.get_basename()
 var current_chunk_id: Vector2i = Vector2i (0,0):
-	set(value):
-		if current_chunk_id.x != value.x and current_chunk_id.y != value.y:
-			current_chunk_id = value
+	set(new_chunk):
+		if current_chunk_id.x != new_chunk.x or current_chunk_id.y != new_chunk.y:
+
+			var the_chunk = Rect2i((chunk_off_set + new_chunk).x
+								,(chunk_off_set + new_chunk).y
+								,(render_distance ).x
+								,(render_distance ).y)
 			#print(current_chunk_id)
 			#print(current_chunk_id)
 			for x in render_distance.x:
 				for y in render_distance.y:
-					var chunk_id = Vector2i (x,y)  + off_set + current_chunk_id
+					var chunk_id = Vector2i (x,y)  + chunk_off_set + new_chunk
+					
+					if x == 1 and y == 1:
+						#print(chunk_id)
+						pass
 					
 					
 					if get_cell_tile_data(chunk_id) == null:
 						set_cell(chunk_id,0,Vector2i(0,0))
 						load_chunk(chunk_id)
-					else :
-						set_cell(chunk_id)
+					
+					var old_chunk = Vector2i (x,y)  + chunk_off_set + current_chunk_id
+					if not the_chunk.has_point(old_chunk):
+						set_cell( old_chunk)
 						deload_chunk(chunk_id)
+			current_chunk_id = new_chunk
+
 
 
 func _ready():
 	player.connect("position_changed",global_to_cell_id)
-	self.tile_set.tile_size = chunk_size * tile_ratio
-	
+	tile_set.tile_size = chunk_size * tile_ratio
+	#print(tile_set.tile_size)
 	
 	for layer in layers:
 		layers_data.append(layer.duplicate())
@@ -46,10 +58,10 @@ func _ready():
 		var temp_size = layer.tile_set.tile_size
 		layer.tile_set = tile_set.duplicate()
 		layer.clear()
+		
 		layer.tile_set.tile_size = temp_size
-		
-		
-	
+
+	pass
 
 
 func global_to_cell_id(global):
@@ -58,23 +70,31 @@ func global_to_cell_id(global):
 func load_chunk(chunk_id: Vector2i):
 	for x in chunk_size.x:
 		for y in chunk_size.y:
-			var temp_cords = chunk_size * tile_ratio * chunk_id  + Vector2i(x,y)
+			var temp_global_cords = chunk_size * tile_ratio * chunk_id  + Vector2i(x,y) * tile_ratio
 			
 
 			for index in len(layers):
-				var temp_atlas_cords = layers_data[index].get_cell_atlas_coords(temp_cords)
-				var temp_source_id = layers_data[index].get_cell_source_id(temp_cords)
-				var temp_temp_cords = layers_data[index].local_to_map(temp_cords)
+				var temp_map_cords = layers_data[index].local_to_map(temp_global_cords)
+				#print(temp_temp_cords)
+				if chunk_id == Vector2i(0,0):
+					#print(chunk_id)
+					print(temp_global_cords )
+					print(temp_map_cords)
+					print("!!!!")
+				var temp_atlas_cords = layers_data[index].get_cell_atlas_coords(temp_map_cords)
+				var temp_source_id = layers_data[index].get_cell_source_id(temp_map_cords)
+				
+				
 				
 				if x== 1 and y == 1 and index == 2:
-					print(temp_temp_cords)
-
-					print(layers_data[index].get_used_cells())
+					#print(temp_temp_cords)
+#
+					#print(layers_data[index].get_used_cells())
 					#
 					#print(temp_atlas_cords)
 					#print(temp_source_id)
 					pass
-				layers[index].set_cell(temp_temp_cords,temp_source_id,temp_atlas_cords)
+				layers[index].set_cell(temp_map_cords,temp_source_id,temp_atlas_cords)
 	#
 	#
 	
