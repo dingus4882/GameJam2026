d875a66145c2bfe5c35abf0d911cb96c644622ea
Jan Ole Petersen <janole.petersen@gmx.de>
2026-07-16 13:18:35 +0200

    screen shake disabler option

--- Main/UI/Settings/settings.gd
+++ Main/UI/Settings/settings.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var music_bar = $"Music Volume"
 @onready var sound_bar = $"Sound Volume"
 
+@onready var screen_shake = $ScreenShake
+
 var is_in_game : bool = false
 
 func _ready():
@@ -16,6 +18,10 @@ func _ready():
 
 	sound_bar.value = VariableController.sound
 	sound_bar.value_changed.connect(_on_sound_changed)
+	
+	if VariableController.screen_shake_enabled:
+		screen_shake.text = "Enabled"
+	else: screen_shake.text = "Disabled"
 
 func _on_music_changed(new_value: float) -> void:
 	VariableController.music = new_value
@@ -29,3 +35,10 @@ func ingame_return():
 	self.visible = false
 func ingame_show():
 	self.visible = true
+
+func toggle_screenshake():
+	var current = VariableController.screen_shake_enabled
+	VariableController.screen_shake_enabled = !current
+	if VariableController.screen_shake_enabled:
+		screen_shake.text = "Enabled"
+	else: screen_shake.text = "Disabled"
